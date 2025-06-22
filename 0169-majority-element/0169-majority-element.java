44c265f568a844bc24e5761184662880bb793597
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-06-22 13:54:47 +0530

    Time: 15 ms (22.67%), Space: 49.5 MB (83.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        return majorityElementByMap(nums);
+        // return majorityElementBySort(nums);
+    }
+
+    public int majorityElementByMap(int[] nums) {
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
@@ -21,5 +26,35 @@ class Solution {
         }
     return ans;
 
+    }
+
+
+    public int majorityElementBySort(int[] nums) {
+        int ans=-1;
+        int max=1;
+        int n= nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        int m=n/2;
+
+
+        Arrays.sort(nums);
+
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                max++;
+            }
+            else{
+                max=1;
+            }
+
+            if(max>n/2)
+            {
+                return nums[i];
+            }
+        }
+
+        return ans;
     }
 }
