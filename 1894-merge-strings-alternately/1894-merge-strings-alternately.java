103647a7661a65e09dcfc588c4be3e2df7395945
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-07-03 23:23:14 +0530

    Time: 1 ms (81.62%), Space: 41.6 MB (93.37%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -2,16 +2,25 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         int l1=word1.length();
         int l2=word2.length();
-        String ans="";
+        StringBuilder ans=new StringBuilder(l1 + l2);
+
+        // uses string concatenation (ans += ...) repeatedly inside a loop, which is inefficient because strings are immutable in Java. Every time you concatenate, a new string is created, leading to unnecessary time and memory overhead.
+
+        //using StringBuilder
+        // Avoids creating new string objects on each concatenation. 
+        // More memory-efficient. 
+        // Faster in time complexity, especially for large inputs.
+
+
         int i=0,j=0;
         while(i<l1 || j<l2){
             if(i<l1){
-                ans+=word1.charAt(i++);
+                ans.append(word1.charAt(i++));
             }
             if(j<l2){
-                ans+=word2.charAt(j++);
+                ans.append(word2.charAt(j++));
             }
         }
-        return ans;
+        return ans.toString();
     }
 }
