777289697bc725e462faa3b44a7605700a65497d
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-07-03 22:59:06 +0530

    Time: 98 ms (85.49%), Space: 67.7 MB (72.22%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -7,20 +7,37 @@ class NumMatrix {
 
         SubMatrix = new int[r + 1][c + 1];
 
+        //prefix sum in row
         for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                SubMatrix[i][j]=matrix[i][j];
+            for(int j=1;j<c;j++){
+                matrix[i][j]+=matrix[i][j-1];
             }
         }
+
+        //prefix sum in coloumn
+        for(int j=0;j<c;j++){
+            for(int i=1;i<r;i++){
+                matrix[i][j]+=matrix[i-1][j];
+            }
+        }
+
+        SubMatrix=matrix;
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++){
-            for(int j=col1;j<=col2;j++){
-                sum+=SubMatrix[i][j];
-            }
+        int rowExtra=0,colExtra=0,common2timeRemoved=0;
+
+        if(row1==0 && col1!=0){
+            colExtra=SubMatrix[row2][col1-1];
+        }else if(col1==0 && row1!=0){
+            rowExtra=SubMatrix[row1-1][col2];
+        }else if(col1!=0 && row1!=0){
+            rowExtra=SubMatrix[row1-1][col2];
+            colExtra=SubMatrix[row2][col1-1];
+            common2timeRemoved=SubMatrix[row1-1][col1-1];
         }
+
+        int sum=SubMatrix[row2][col2]-rowExtra-colExtra+common2timeRemoved;
         return sum;
     }
 }
