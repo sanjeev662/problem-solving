7e489e0e678f6613b7f5344a52890f5076afab01
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-07-06 13:44:53 +0530

    Time: 14 ms (70.33%), Space: 43.6 MB (24.48%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,50 +1,31 @@
 class Solution {
-    public int threeSumClosest(int[] nums, int realtarget) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        int closestSum=0;
-        int diff=Integer.MAX_VALUE;
+    public int threeSumClosest(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);       
+        int closestSum = nums[0] + nums[1] + nums[2];
 
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1])
-            continue;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int target= nums[i];
+            int left = i + 1;
+            int right = n - 1;
 
-            int tempans= twoSum(nums,realtarget-target,realtarget,i+1,n-1);
-            int tempdiff= Math.abs(realtarget-tempans);
-            
-            if(tempdiff==0) return tempans;
-            else if(diff>tempdiff){
-                diff=tempdiff;
-                closestSum=tempans;
+            while (left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                // If exact match, return immediately
+                if (currentSum == target) return currentSum;
+
+                // Update closest sum if current sum is closer to target
+                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                    closestSum = currentSum;
+                }
+
+                // Move pointers based on sum comparison
+                if (currentSum < target) left++;
+                else right--;                
             }
         }
-        return closestSum; 
-    }
-
-        public int twoSum(int[] nums, int target,int realtarget,int l,int r){
-            int temp=Integer.MAX_VALUE;
-            int tempans=0;
-        while(l<r){
-            if(target>nums[l]+nums[r]) {
-                    if(temp>target-(nums[l]+nums[r])){
-                        temp=target-(nums[l]+nums[r]);
-                        tempans=nums[l]+nums[r]+(realtarget-target);
-                    }
-                    l++;
-                }
-            else if(target<nums[l]+nums[r]){
-                    if(temp>(nums[l]+nums[r])-target){
-                        temp=(nums[l]+nums[r])-target;
-                        tempans=nums[l]+nums[r]+(realtarget-target);
-                    }
-                    r--;
-                }
-            else{
-                return nums[l]+nums[r]+(realtarget-target);
-            }
-        }
-        return tempans;
+        return closestSum;
     }
 }
