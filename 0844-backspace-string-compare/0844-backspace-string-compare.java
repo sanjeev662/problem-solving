38ccb1d2739a04b17537d08342773bb1ca7d7fbf
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-12-23 14:16:00 +0530

    Time: 0 ms (100%), Space: 42.6 MB (82.29%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,23 +1,76 @@
+// class Solution {
+//     public boolean backspaceCompare(String s, String t) {
+//         return finalString(s).equals(finalString(t));
+//     }
+
+//     private String finalString(String str) {
+//         StringBuilder sb = new StringBuilder();
+
+//         for (int i = 0; i < str.length(); i++) {
+//             char ch = str.charAt(i);
+
+//             if (ch == '#') {
+//                 if (sb.length() > 0) {
+//                     sb.deleteCharAt(sb.length() - 1);
+//                 }
+//             } else {
+//                 sb.append(ch);
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
+
+//Two-Pointer Solution (O(1) Space)
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return finalString(s).equals(finalString(t));
-    }
+        int i = s.length() - 1;
+        int j = t.length() - 1;
 
-    private String finalString(String str) {
-        StringBuilder sb = new StringBuilder();
+        int skipS = 0;
+        int skipT = 0;
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
+        while (i >= 0 || j >= 0) {
 
-            if (ch == '#') {
-                if (sb.length() > 0) {
-                    sb.deleteCharAt(sb.length() - 1);
+            // Find next valid char in s
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
                 }
-            } else {
-                sb.append(ch);
             }
+
+            // Find next valid char in t
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            // Compare characters
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) != t.charAt(j)) return false;
+            } else {
+                // One string ended before the other
+                if (i >= 0 || j >= 0) return false;
+            }
+
+            i--;
+            j--;
         }
 
-        return sb.toString();
+        return true;
     }
 }
