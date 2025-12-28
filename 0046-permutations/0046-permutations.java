b047791bbd482ae1035d6d1b1606d392ceb97189
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-12-28 20:31:16 +0530

    Time: 4 ms (4.96%), Space: 46.7 MB (5.11%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        ans.add(new ArrayList<>());
+
+        for(int i=0;i<nums.length;i++){
+            int len=ans.size();
+            for(int j=0;j<len;j++){
+                List<Integer> ls=new LinkedList<>(ans.get(j));
+                int lslen=ls.size();
+
+                for (int ii = 0; ii <= lslen; ii++) {
+                    ls.add(ii, nums[i]);              // insert at index ii
+                    ans.add(new LinkedList<>(ls));    // copy
+                    ls.remove(ii);                    // 🔴 MUST undo (backtrack)
+                }
+            }
+        }
+
+        List<List<Integer>> finalans=new ArrayList<>();
+        for(List<Integer> arr: ans){
+            if(arr.size()==nums.length)
+            finalans.add(arr);
+        }
+
+        return finalans;
+    }
+}
