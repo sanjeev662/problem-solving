163c6beb57f948d1029b5982d2d3a79424ce50ce
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-12-28 20:34:39 +0530

    Time: 2 ms (40.08%), Space: 46.4 MB (7.09%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,3 +1,34 @@
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> ans=new ArrayList<>();
+//         ans.add(new ArrayList<>());
+
+//         for(int i=0;i<nums.length;i++){
+//             int len=ans.size();
+//             for(int j=0;j<len;j++){
+//                 List<Integer> ls=new LinkedList<>(ans.get(j));
+//                 int lslen=ls.size();
+
+//                 for (int ii = 0; ii <= lslen; ii++) {
+//                     ls.add(ii, nums[i]);              // insert at index ii
+//                     ans.add(new LinkedList<>(ls));    // copy
+//                     ls.remove(ii);                    // 🔴 MUST undo (backtrack)
+//                 }
+//             }
+//         }
+
+//         List<List<Integer>> finalans=new ArrayList<>();
+//         for(List<Integer> arr: ans){
+//             if(arr.size()==nums.length)
+//             finalans.add(arr);
+//         }
+
+//         return finalans;
+//     }
+// }
+
+
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
@@ -6,23 +37,17 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             int len=ans.size();
             for(int j=0;j<len;j++){
-                List<Integer> ls=new LinkedList<>(ans.get(j));
-                int lslen=ls.size();
+                List<Integer> oldls=ans.remove(0);
+                int oldlen=oldls.size();
 
-                for (int ii = 0; ii <= lslen; ii++) {
-                    ls.add(ii, nums[i]);              // insert at index ii
-                    ans.add(new LinkedList<>(ls));    // copy
-                    ls.remove(ii);                    // 🔴 MUST undo (backtrack)
+                for (int ii = 0; ii <= oldlen; ii++) {
+                    oldls.add(ii, nums[i]);              // insert at index ii
+                    ans.add(new LinkedList<>(oldls));    // copy
+                    oldls.remove(ii);                    // 🔴 MUST undo (backtrack)
                 }
             }
         }
-
-        List<List<Integer>> finalans=new ArrayList<>();
-        for(List<Integer> arr: ans){
-            if(arr.size()==nums.length)
-            finalans.add(arr);
-        }
-
-        return finalans;
+        
+        return ans;
     }
 }
