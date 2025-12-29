f7664b09eb52780f7a99137c56727ebfc3b84952
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2025-12-29 19:53:34 +0530

    Time: 4 ms (37.83%), Space: 47.2 MB (68.04%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -1,3 +1,35 @@
+// // class Solution {
+// //     public List<String> letterCasePermutation(String s) {
+// //         List<String> ls=new ArrayList<>();
+// //         ls.add(s);
+// //         int n=s.length();
+
+// //         for(int i=0;i<n;i++){
+// //             char ch=s.charAt(i);
+// //             int len=ls.size();
+// //             if (ch >= 'a' && ch <= 'z'){
+// //                 for(int j=0;j<len;j++){
+// //                     String st = ls.get(j);
+// //                     StringBuilder sb = new StringBuilder(st);
+// //                     sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
+// //                     ls.add(sb.toString());
+// //                 }
+// //             }else if(ch >= 'A' && ch <= 'Z'){
+// //                 for(int j=0;j<len;j++){
+// //                     String st = ls.get(j);
+// //                     StringBuilder sb = new StringBuilder(st);
+// //                     sb.setCharAt(i, Character.toLowerCase(sb.charAt(i)));
+// //                     ls.add(sb.toString());
+// //                 }
+// //             }else{
+// //                 continue;
+// //             }
+// //         }
+// //         return ls;
+// //     }
+// // }
+
+
 // class Solution {
 //     public List<String> letterCasePermutation(String s) {
 //         List<String> ls=new ArrayList<>();
@@ -6,54 +38,50 @@
 
 //         for(int i=0;i<n;i++){
 //             char ch=s.charAt(i);
+
+//             if(!Character.isLetter(ch)) continue;
+
 //             int len=ls.size();
-//             if (ch >= 'a' && ch <= 'z'){
-//                 for(int j=0;j<len;j++){
-//                     String st = ls.get(j);
-//                     StringBuilder sb = new StringBuilder(st);
-//                     sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
-//                     ls.add(sb.toString());
-//                 }
-//             }else if(ch >= 'A' && ch <= 'Z'){
-//                 for(int j=0;j<len;j++){
-//                     String st = ls.get(j);
-//                     StringBuilder sb = new StringBuilder(st);
-//                     sb.setCharAt(i, Character.toLowerCase(sb.charAt(i)));
-//                     ls.add(sb.toString());
-//                 }
-//             }else{
-//                 continue;
+//             for(int j=0;j<len;j++){
+//                 String st = ls.get(j);
+//                 StringBuilder sb = new StringBuilder(st);
+
+//                 if(Character.isUpperCase(sb.charAt(i)))
+//                 sb.setCharAt(i, Character.toLowerCase(sb.charAt(i)));
+//                 else
+//                 sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
+
+//                 ls.add(sb.toString());
 //             }
 //         }
 //         return ls;
 //     }
 // }
 
-
 class Solution {
     public List<String> letterCasePermutation(String s) {
-        List<String> ls=new ArrayList<>();
-        ls.add(s);
-        int n=s.length();
 
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
+        List<String> ans = new ArrayList<>();
+        ans.add(s);
 
-            if(!Character.isLetter(ch)) continue;
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isLetter(s.charAt(i))) continue;
 
-            int len=ls.size();
-            for(int j=0;j<len;j++){
-                String st = ls.get(j);
-                StringBuilder sb = new StringBuilder(st);
+            int size = ans.size();
 
-                if(Character.isUpperCase(sb.charAt(i)))
-                sb.setCharAt(i, Character.toLowerCase(sb.charAt(i)));
-                else
-                sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
+            for (int j = 0; j < size; j++) {
+                char[] chars = ans.get(j).toCharArray();
 
-                ls.add(sb.toString());
+                // toggle case
+                if (Character.isLowerCase(chars[i])) {
+                    chars[i] = Character.toUpperCase(chars[i]);
+                } else {
+                    chars[i] = Character.toLowerCase(chars[i]);
+                }
+
+                ans.add(new String(chars));
             }
         }
-        return ls;
+        return ans;
     }
 }
