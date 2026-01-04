5ad3bcd8a7ae6be4235a2fcf33a27d32c0910ca3
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-01-04 20:03:57 +0530

    Time: 14 ms (68.1%), Space: 47.5 MB (66.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,39 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> mp=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int num : nums) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
         }
 
-        // PriorityQueue<Map.Entry<Integer,Integer>> heap=new PriorityQueue<>((a,b)-> a.getValue()-b.getValue());
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 
-        // for(Map.Entry<Integer,Integer> map : mp.entrySet()){
-        //     heap.add(map);
-        //     if(heap.size()>2){
-        //         heap.poll();
-        //     }
-        // }
-
-        // int[] ans= new int[k];
-        // for(int i=0;i<k;i++){
-        //     ans[i] = heap.poll().getKey();
-        // }
-
-        PriorityQueue<Integer> heap= new PriorityQueue<>(
-            (a,b) -> mp.get(a)-mp.get(b)
-        );
-
-        for(Map.Entry<Integer,Integer> map : mp.entrySet()){
-
-            heap.add(map.getKey());
-            if(heap.size()>k){
-                heap.poll();
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) {
+                pq.poll();
             }
         }
 
-        int[] ans= new int[k];
-        for(int i=0;i<k;i++){
-            ans[i]=heap.poll();
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll().getKey();
         }
 
         return ans;
