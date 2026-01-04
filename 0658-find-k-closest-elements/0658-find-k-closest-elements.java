c95c52e51da04a25c914f537b2eec74f57ce6937
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-01-04 21:42:49 +0530

    Time: 4 ms (98.24%), Space: 47.8 MB (63.57%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,26 +1,49 @@
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(
+//         (a, b) -> {
+//             int diff = Math.abs(b - x) - Math.abs(a - x);
+//             if (diff == 0) return b - a;
+//             return diff;
+//             }
+//         );
+
+//         for(int i=0;i<arr.length;i++){
+//             pq.offer(arr[i]);
+
+//             if(pq.size()>k)
+//             pq.poll();
+//         }
+
+//         List<Integer> ans = new ArrayList<>();
+//         for(int i=0;i<k;i++){
+//             ans.add(pq.poll());
+//         }
+//         Collections.sort(ans);
+//         return ans;
+//     }
+// }
+
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0, right = arr.length - k;
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-        (a, b) -> {
-            int diff = Math.abs(b - x) - Math.abs(a - x);
-            if (diff == 0) return b - a;
-            return diff;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
-        );
-
-        for(int i=0;i<arr.length;i++){
-            pq.offer(arr[i]);
-
-            if(pq.size()>k)
-            pq.poll();
         }
 
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<k;i++){
-            ans.add(pq.poll());
+        List<Integer> result = new ArrayList<>();
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
         }
-        Collections.sort(ans);
-        return ans;
+
+        return result;
     }
 }
