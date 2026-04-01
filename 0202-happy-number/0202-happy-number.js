f9cd0a37b0ed2cb2d372703011bb20cff58d5541
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-04-01 20:32:35 +0530

    Time: 0 ms (100%), Space: 53.7 MB (96.01%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -3,17 +3,40 @@
  * @return {boolean}
  */
 
+// var isHappy = function(n) {
+//     let seen = new Set();
+
+//     while (!seen.has(n)) {
+//         if (n === 1) return true;
+//         seen.add(n);
+
+//         n = n.toString()
+//              .split('')
+//              .reduce((sum, d) => sum + d * d, 0);
+//     }
+
+//     return false;
+// };
+
 var isHappy = function(n) {
-    let seen = new Set();
 
-    while (!seen.has(n)) {
-        if (n === 1) return true;
-        seen.add(n);
+    const getNext = (num) => {
+        let sum = 0;
+        while (num > 0) {
+            let digit = num % 10;
+            sum += digit * digit;
+            num = Math.floor(num / 10);
+        }
+        return sum;
+    };
 
-        n = n.toString()
-             .split('')
-             .reduce((sum, d) => sum + d * d, 0);
-    }
+    let slow = n;
+    let fast = n;
 
-    return false;
+    do {
+        slow = getNext(slow);           // 1 step
+        fast = getNext(getNext(fast));  // 2 steps
+    } while (slow !== fast);
+
+    return slow === 1;
 };
