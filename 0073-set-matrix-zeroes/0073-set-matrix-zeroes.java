4b736b29380deecd6a602e4cc2b9fb770a119c62
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-07-19 01:49:32 +0530

    Time: 4 ms (15.01%), Space: 47.5 MB (81.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,37 +2,27 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
-
-        Integer[][] temp=new Integer[n][m];
+        Set<Integer> setn=new HashSet<>();
+        Set<Integer> setm=new HashSet<>();
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]!=0){
-                    if(temp[i][j]==null)
-                    temp[i][j]=matrix[i][j];
-                }else{
-                    fillNewZero(temp,i,j,n,m);
+                if(matrix[i][j]==0){
+                    setn.add(i);
+                    setm.add(j);
                 }
             }
         }
 
-        clone(matrix,temp,n,m);
+        fillNewZero(matrix,setn,setm,n,m);
     }
 
-    public void fillNewZero(Integer[][] temp,int ii,int jj, int nn, int mm){
-        for(int i=0;i<nn;i++){
-            for(int j=0;j<mm;j++){
-                if(i==ii || j==jj){
-                    temp[i][j]=0;
-                }
-            }
-        }
-    }
-
-    public void clone(int[][] matrix,Integer[][] temp,int n, int m){
+    public void fillNewZero(int[][] matrix,Set<Integer> setn,Set<Integer> setm, int n, int m){
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                    matrix[i][j]=temp[i][j];
+                if(setn.contains(i) || setm.contains(j)){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
