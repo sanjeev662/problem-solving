ad73ba96112c8774e4c9490c7e7d8a79bdefb0e9
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-07-19 02:26:01 +0530

    Time: 1 ms (98.46%), Space: 47.5 MB (62.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,28 +2,38 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
-        Set<Integer> setn=new HashSet<>();
-        Set<Integer> setm=new HashSet<>();
+
+        boolean firstColumnZero=false;
+        boolean firstRowZero=false;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    setn.add(i);
-                    setm.add(j);
+                    if(i==0) firstRowZero=true;
+                    if(j==0) firstColumnZero=true;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        fillNewZero(matrix, firstRowZero, firstColumnZero, n, m);
+    }
+
+    public void fillNewZero(int[][] matrix, boolean firstRowZero, boolean firstColumnZero, int n, int m){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
                 }
             }
         }
 
-        fillNewZero(matrix,setn,setm,n,m);
-    }
+        if(firstRowZero){
+            for(int j=0;j<m;j++){matrix[0][j]=0;}
+        }
 
-    public void fillNewZero(int[][] matrix,Set<Integer> setn,Set<Integer> setm, int n, int m){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(setn.contains(i) || setm.contains(j)){
-                    matrix[i][j]=0;
-                }
-            }
+        if(firstColumnZero){
+            for(int i=0;i<n;i++){matrix[i][0]=0;}
         }
     }
 }
