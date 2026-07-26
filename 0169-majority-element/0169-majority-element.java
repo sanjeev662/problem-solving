7b51f95cc1e2d46b4fc787fbf0408420dc2889ab
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-07-26 12:10:47 +0530

    Time: 17 ms (16.46%), Space: 52.5 MB (90.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,60 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // return majorityElementByMap(nums);
-        return majorityElementBySort(nums);
-    }
+        int n=nums.length;
+        int majority=n/2;
 
-    public int majorityElementByMap(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }else{
-                map.put(nums[i],1);
-            }
+        Map<Integer,Integer> mp=new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            if(mp.get(nums[i])>majority)
+            return nums[i];
         }
-
-        int ans =0;
-        int max=0;
-
-        for(Integer key: map.keySet()){
-            Integer value=map.get(key);
-            if(max<value){
-                ans=key;
-                max=value;
-            }
-        }
-    return ans;
-
-    }
-
-
-    public int majorityElementBySort(int[] nums) {
-        int ans=-1;
-        int max=1;
-        int n= nums.length;
-        if(n==1){
-            return nums[0];
-        }
-        int m=n/2;
-
-
-        Arrays.sort(nums);
-
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                max++;
-            }
-            else{
-                max=1;
-            }
-
-            if(max>n/2)
-            {
-                return nums[i];
-            }
-        }
-
-        return ans;
+        return nums[0];
     }
 }
