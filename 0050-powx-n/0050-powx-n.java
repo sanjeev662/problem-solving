b208b81634186c37725705f88a355af1a29b8c03
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-07-26 10:35:23 +0530

    Time: 0 ms (100%), Space: 47.9 MB (65.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,25 +1,48 @@
 class Solution {
-    public double myPow(double x, int num) {
-        double ans=1.0;
-        long n=num;
+    // public double myPow(double x, int n) {
+    //     long temp=n;
+
+    //     if(n==0 || x==1) return 1;
+
+    //     if(n<0){
+    //         x=1/x;
+    //         temp=-1L*temp;
+    //     }
+
+    //     double ans=1;
+    //     for(int i=0;i<temp;i++){
+    //         ans=ans*x;
+    //     }
+    //     return ans;
+    // }
 
 
-        if(n==0) return 1.0;
+    public double myPow(double x, int n) {
+        if(n==0 || x==1) return 1;
+        double ans=1;
+        long temp=n;
 
         if(n<0){
-            n=-(n);
-            x=1/x;
+            temp=-1L*n;
         }
 
-        while(n>0){
-            if(n%2==1){
-                ans=ans*x;
-            }
-            x*=x;
-            n=n/2;
+        ans=power(x,temp);
+
+        if(n<0){
+            return 1/ans;
         }
 
         return ans;
-
     }
+
+    public double power(double x, long n){
+        if(n==0) return 1;
+        if(n==1) return x;
+
+        if(n%2==0)
+        return power(x*x,n/2);
+        
+        return x*power(x,n-1);
+    }
+
 }
