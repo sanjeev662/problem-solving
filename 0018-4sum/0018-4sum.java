eddeb1add7e804f5b39ca271d8165b0d0194bff9
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-07-30 00:12:08 +0530

    Time: 20 ms (65.74%), Space: 45.4 MB (95.06%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,39 +1,47 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans= new ArrayList<>();
-        int n=nums.length;
         Arrays.sort(nums);
-
+        int n=nums.length;
+        
+        List<List<Integer>> ans=new ArrayList<>();
+        
         for(int i=0;i<n-3;i++){
-            if(i!=0 && nums[i]==nums[i-1])
-            continue;
 
-            for(int j=i+1;j<n-2;j++){
-                if(j!=i+1 && nums[j]==nums[j-1])
+            if(i>0){
+                if(nums[i]==nums[i-1])
                 continue;
-                
-                int l=j+1;
-                int r=n-1;
-
-                while(l<r){
-                    long temp=nums[i]+nums[j];
-                    temp+=nums[l];
-                    temp+=nums[r];
-
-                    if(target<temp) r--;
-                    else if(target>temp) l++;
-                    else{
-                        while(l<r && nums[l]==nums[l+1]) l++;
-                        while(l<r && nums[r]==nums[r-1]) r--;
-
-                        ans.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
-                        l++;
-                        r--;
-                    }
+            }
+            
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1){
+                    if(nums[j]==nums[j-1])
+                    continue;
                 }
+                twoSum(nums,(long)target,i,j,ans);
             }
         }
-
         return ans;
+    }
+
+    public void twoSum(int[] nums,long target,int i, int j,List<List<Integer>> ans){
+        int n=nums.length;
+        int left=j+1;
+        int right=n-1;
+        while(left<right){
+            long sum=(long) nums[i]+nums[j]+nums[left]+nums[right];
+            if(sum==target){
+                ans.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+                left++;
+                right--;
+
+                while(left<right && nums[left]==nums[left-1]) left++;
+                while(left<right && nums[right]==nums[right+1]) right--;
+            }
+            else if(sum>target){
+                right--;
+            }else{
+                left++;
+            }            
+        }
     }
 }
