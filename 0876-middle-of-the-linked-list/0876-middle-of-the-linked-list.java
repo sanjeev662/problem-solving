2682a1ddbc234f7fee281e81b78dad7e320fc3ae
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-07-31 11:08:27 +0530

    Time: 0 ms (100%), Space: 42.9 MB (54.68%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -14,9 +14,10 @@ class Solution {
         ListNode fast=head;
 
         while(fast!=null && fast.next!=null){
-            slow=slow.next;
+            slow=slow.next;          
             fast=fast.next.next;
         }
+
         return slow;
     }
 }
