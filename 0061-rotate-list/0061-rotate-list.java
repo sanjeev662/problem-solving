bb1ec6a0d8e2af29e6906712fcb7a239a90712c6
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-07-31 20:06:17 +0530

    Time: 0 ms (100%), Space: 44.5 MB (26.29%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,84 +1,45 @@
-// /**
-//  * Definition for singly-linked list.
-//  * public class ListNode {
-//  *     int val;
-//  *     ListNode next;
-//  *     ListNode() {}
-//  *     ListNode(int val) { this.val = val; }
-//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//  * }
-//  */
-// class Solution {
-//     public ListNode rotateRight(ListNode head, int k) {
-//         if(head==null || head.next==null || k==0) return head;
-//         ListNode curr=head;
-
-//         //for getting length then actual index from left
-//         int len=0;
-//         ListNode temp=head;
-//         while(temp != null){
-//             temp=temp.next;
-//             len++;
-//         }
-//         int actualInd=len-(k%len);
-
-//         if(actualInd==len) return head; //rotated list will same
-
-//         ListNode tt = null;
-//         while(curr!=null && actualInd>0){
-//             actualInd--;
-//             if(actualInd == 0){
-//                 tt=curr.next;
-//                 curr.next=null;
-//                 break;
-//             }
-//             curr=curr.next;
-//         }
-
-//         ListNode ans=tt;
-//         while(tt.next!=null){
-//             tt=tt.next;
-//         }
-
-//         tt.next=head;
-
-//         return ans;
-//     }
-// }
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) return head;
+        if(head==null || head.next==null || k==0) return head;
+        ListNode curr=head;
+        ListNode newhead=head;
+        ListNode len=head;
 
-        // Step 1: find length and tail
-        int len = 1;
-        ListNode tail = head;
-
-        while (tail.next != null) {
-            tail = tail.next;
-            len++;
+        int n=0;
+        while(len!=null){
+            len=len.next;
+            n++;
         }
 
-        // Step 2: normalize k
-        k = k % len;
-        if (k == 0) return head;
+        if(k%n==0) return head;
 
-        // Step 3: find new tail (len - k - 1 steps from head)
-        int stepsToNewTail = len - k;
-        ListNode curr = head;
+        int nn=n-(k%n);
 
-        for (int i = 1; i < stepsToNewTail; i++) {
-            curr = curr.next;
+        while(curr!=null){
+            nn--;
+            ListNode temp=null;
+            if(nn==0){
+                newhead=curr.next;
+                curr.next=null;
+                ListNode tt=newhead;
+                while(tt!=null && tt.next!=null){
+                    tt=tt.next;
+                }
+                tt.next=head;
+            }
+            curr=curr.next;
         }
 
-        // Step 4: rotate
-        ListNode newHead = curr.next;
-        curr.next = null;
-        tail.next = head;
-
-        return newHead;
+        return newhead;
     }
 }
-
-// I first compute the length and tail, normalize k, find the new tail at position len - k, break the list there, and reconnect the old tail to the original head.
-
