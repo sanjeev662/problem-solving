786547cf33b0926d4f4fd30ef903077c060e5830
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-08-03 11:24:03 +0530

    Time: 2 ms (99.72%), Space: 45.3 MB (24.47%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        ans.add(new ArrayList<>());
         Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
 
-        int start=0;
-        int end=ans.size();
 
-        for(int i=0;i<nums.length;i++){
-            start=0;
+        backtrack(0, nums, temp, ans);
 
-            if(i>0 && nums[i]==nums[i-1]){
-                start=end;
-            }
-            end=ans.size();
-
-            for(int j=start;j<end;j++){
-                List<Integer> ls=new ArrayList<>(ans.get(j));
-                ls.add(nums[i]);
-                ans.add(ls);
-            }
-        }
         return ans;
+    }
+
+    public void backtrack(int start,int[] nums,List<Integer> temp, List<List<Integer>> ans){
+
+        ans.add(new ArrayList<>(temp));
+        
+        for(int i=start;i<nums.length;i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+
+            // Choose
+            temp.add(nums[i]);
+
+            // Explore
+            backtrack(i + 1, nums, temp, ans);
+
+            // Undo (Backtrack)
+            temp.remove(temp.size() - 1);
+        }
     }
 }
