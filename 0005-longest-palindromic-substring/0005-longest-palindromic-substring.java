ac78747ce32d4de7192eaef694ce7023b3af258e
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-08-07 00:10:40 +0530

    Time: 19 ms (40.16%), Space: 47.1 MB (17.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,36 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int maxLength=0;
-        int startInd=0;
         int n=s.length();
+        if(n<=1) return s;
 
-        int[][] dp=new int[n+1][n+1];
-        //consider 0 not approached, 1 true, 2 false
+        int max=0;
+        String ans=""+s.charAt(0);
 
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindromic(s,i,j,dp)){
-                    int len=j-i;
-                    if(len > maxLength){
-                        maxLength=len;
-                        startInd=i;
-                    }
+            int j=i;
+            int k=i;
+            while(j>=0 && k<n && s.charAt(j)==s.charAt(k)){
+                int diff=k-j;
+                if(diff>max){
+                    max=diff;
+                    ans=s.substring(j,k+1);
                 }
+                j--;
+                k++;
+            }
+
+            j=i;
+            k=i+1;
+            while(j>=0 && k<n && s.charAt(j)==s.charAt(k)){
+                int diff=k-j;
+                if(diff>max){
+                    max=diff;
+                    ans=s.substring(j,k+1);
+                }
+                j--;
+                k++;
             }
         }
-        return s.substring(startInd,startInd+maxLength+1);
-    }
-
-    public boolean isPalindromic(String s,int i,int j,int[][] dp){
-        if(i>=j) return true;
-
-        if(dp[i][j]!=0) return dp[i][j]==1?true:false;
-
-        if(s.charAt(i)==s.charAt(j))
-        {
-          boolean temp=isPalindromic(s,i+1,j-1,dp);
-            dp[i][j]= temp?1:2;
-          return temp;
-        }
-
-        dp[i][j]=2;
-        return false;
+        return ans;
     }
 }
