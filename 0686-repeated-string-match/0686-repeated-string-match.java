a53a81b2ab63a9355cb17754b85567fd760afa64
Sanjeev Kumar Singh <94432263+sanjeev662@users.noreply.github.com>
2026-08-08 23:58:11 +0530

    Time: 262 ms (55.13%), Space: 43.8 MB (33.15%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -6,22 +6,17 @@ class Solution {
         int match=nb/na;
         int count=match;
         StringBuilder ss=new StringBuilder("");
-
-        if(nb%na==0 && a.charAt(0)==b.charAt(0)){           
-            while(count>0){
-             ss.append(a);
-             count--;
-            }
-            if(ss.toString().contains(b)) return match;
-        }
-
-        count=match+1;
+        
         while(count>0){
              ss.append(a);
              count--;
         }
+        
+        if(nb%na==0 && ss.toString().contains(b)) return match;
+        ss.append(a);
         if(ss.toString().contains(b)) return match+1;
-        if(ss.append(a).toString().contains(b)) return match+2;
+        ss.append(a);
+        if(ss.toString().contains(b)) return match+2;
 
         return -1;
     }
